e1ee870fc12b5c8088d22cbab69ddbf2de4e7c2f
tuanbui0509 <tuanbui0509@gmail.com>
2021-07-05 14:27:21 +0700

    fix home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,3 +102,18 @@ scrolls.reveal(`
             .footer__content`,{
     interval: 200
 })
+
+/*==================== CHANGE TABS CATEGORY ====================*/
+changeTab = (event, tab) => {
+    let i, tabContent, tabLinks
+    tabContent = document.getElementsByClassName('organically__pills')
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabs-item")
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace("tabs-item--active", "")
+    }
+    document.getElementById(tab).style.display = 'block'
+    event.currentTarget.className += ' tabs-item--active'
+}
